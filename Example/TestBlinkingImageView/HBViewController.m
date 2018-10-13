4ab60d27593f60259ff44780f8839f95f55d6a6b
Libs <1103785815@qq.com>
2018-10-13 14:42:30 +0800

    Initial commit

---
+++ Example/TestBlinkingImageView/HBViewController.m
@@ -0,0 +1,29 @@
+//
+//  HBViewController.m
+//  TestBlinkingImageView
+//
+//  Created by 1103785815@qq.com on 10/13/2018.
+//  Copyright (c) 2018 1103785815@qq.com. All rights reserved.
+//
+
+#import "HBViewController.h"
+
+@interface HBViewController ()
+
+@end
+
+@implementation HBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
